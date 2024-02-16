87f89a42ab8d86db792da5dfc511e518208d8044
sergej-maric <sergejmaric01@gmail.com>
2024-02-16 20:38:55 +0100

    Na pola puta

---
+++ script.js
@@ -0,0 +1,14 @@
+function openCloseMenu(){
+    let menu = document.getElementById('menu');
+
+    if(!!menu && menu.style.display === "none")
+    {
+        menu.style.display = 'block';
+    }
+    else{
+        menu.style.display = 'none';
+    }
+
+    
+    console.log(menu);
+}
